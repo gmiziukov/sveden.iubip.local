8dc9bf1ec5e912a951147a48316ef164ad0baf04
divas209 <divasis209@gmail.com>
2024-10-31 15:11:27 +0300

    education

---
+++ app/Models/Education_page_table.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Education_page_table extends Model
+{
+    use HasFactory;
+}
