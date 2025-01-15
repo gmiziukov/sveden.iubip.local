f69e326a42ebd89215f8fef13e3748edf6a4e497
Divas209 <116635087+Divas209@users.noreply.github.com>
2025-01-16 02:57:50 +0300

    remig_service

--- app/Console/Commands/remig.php
+++ app/Console/Commands/remig.php
@@ -2,88 +2,74 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Database\Migrations\MigrationCreator as mc;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Filesystem\Filesystem;
-
 use Illuminate\Console\Command;
-// use Illuminate\Database\ConnectionResolverInterface as Resolver;
-// use Illuminate\Database\Migrations\DatabaseMigrationRepository as dmr;
-
+use App\Services\MyMigrationCreator;
 
 
 class remig extends Command implements PromptsForMissingInput
-
 {
-    // protected $laravel;
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'app:remig {name : The name of the migration}
-        {--table= : The table to migrate}
-        {--create= : The table to be created}
-        {--path= : The location where the migration file should be created}';
-    
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'create stub migration';
-    
-    /**
-     * Execute the console command.
-     * 
-     */
-    
-    protected $table;
-    protected $path;
-    protected $name;
+                            {table? : The table to migrate}
+                            {create? : The table to be created}
+                            {--path= : The location where the migration file should be created}';
+
+    protected $description = 'Create a stub migration.';
+
+    protected $stubPath;
+
+    protected $mc;
 
     public function __construct(Filesystem $file)
     {
         parent::__construct();
-        $this->mc = new mc($file , $customStubPath = "\Illuminate\Database\Migrations\stubs\\newstub\migration.stub" );
+        $this->stubPath = realpath(__DIR__ . '/../../Database/Migrations/stubs/newstub');
+
+          $this->mc = new MyMigrationCreator($file, $this->stubPath);
     }
-    
+
     protected function getMigrationPath()
     {
-        if (! is_null($targetPath = $this->input->getOption('path'))) {
-            return ! $this->usingRealPath()
-            ? $this->laravel->basePath().'/'.$targetPath
-            : $targetPath;
+        $targetPath = $this->option('path');
+
+        if (is_null($targetPath)) {
+            $targetPath = $this->laravel->databasePath('migrations');
         }
-        
-        return parent::getMigrationPath();
+
+        return $targetPath;
     }
-    protected function writeMigration($name, $table, $create)
-    {
-        $file = $this->creator->create(
-            $name, $this->getMigrationPath(), $table, $create
-        );
-        
-        $this->components->info(sprintf('Migration [%s] created successfully.', $file));
-    }
-    protected function promptForMissingArgumentsUsing(): array
-    {
-        return [
-            'user' => 'Which user ID should receive the mail?',
-        ];
-    }
+
     public function handle()
     {
-        $name = $this->input->getArgument('name');
-        $this->table = $this->input->getOption('table', "def");
-        $create = $this->input->getOption('create') ?: false;
-        if (! $table && is_string($create)) {
-            $table = $create;
+        dump($this->stubPath);
+        $this->name = $this->argument('name');
+        $this->table = $this->argument('table');
+        $create = $this->argument('create');
 
+
+         if ($create && is_string($create)) {
+            $this->table = $create;
             $create = true;
+        }  elseif (!is_null($create)) {
+            $create = (bool) $create;
         }
-        $this->writeMigration($name, $table, $create);
-        // $this->createRepository();
+
+
+        $this->writeMigration($this->name, $this->table, $create);
         return 0;
+    }
+
+    protected function writeMigration($name, $table, $create)
+    {
+       dump("Stub file from command: " . $this->stubPath . '/migration.stub');
+
+        $this->mc->create(
+            $name,
+            $this->getMigrationPath(),
+            $table,
+            null // используем null
+        );
+         $this->info("Migration [$name] created successfully.");
     }
 }
---
+++ app/Services/MyMigrationCreator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Database\Migrations\MigrationCreator;
+use Illuminate\Filesystem\Filesystem;
+
+class MyMigrationCreator
+{
+    protected $files;
+    protected $stubPath;
+    protected $migrationCreator;
+
+
+    public function __construct(Filesystem $files, $stubPath)
+    {
+        $this->files = $files;
+        $this->stubPath = $stubPath;
+        $this->migrationCreator = new MigrationCreator($files, null, false);
+    }
+    /**
+     * Get the stub file for the generator.
+     *
+     * @param  string|null  $table
+     * @param  bool  $create
+     * @return string
+     */
+    protected function getStub($table, $create)
+    {
+        dump($table, $create);
+         if (is_null($table) && !$create) {
+           $stubFile =  realpath($this->stubPath . '/migration.stub');
+       } elseif (!is_null($table) && $create) {
+           $stubFile =  realpath($this->stubPath . '/migration.create.stub');
+        } else {
+          $stubFile = realpath($this->stubPath . '/migration.update.stub');
+        }
+        
+        if (!file_exists($stubFile)) {
+            throw new \Exception("Stub file not found: " . $stubFile);
+        }
+         // Выводим путь до файла шаблона в консоль
+        dump("Stub file path: " . $stubFile);
+        return file_get_contents($stubFile);
+    }
+    public function create($name, $path, $table = null, $create = false)
+    {
+         $stub = $this->getStub($table, $create);
+        $this->migrationCreator->create($name, $path, $table, $stub);
+    }
+}
